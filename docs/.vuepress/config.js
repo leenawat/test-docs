2dab4596463e0cd50ec9093d5cd6fe318bcdc6cc
leenawat <leenawat.pa@gmail.com>
2020-05-08 11:25:07 +0700

    add config

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  base: "/test-docs/",
+  port: 5000,
+  dest: "site",
+  title: 'test-docs',
+  description: 'Everyone can develop projects independently, quickly and efficiently！'
+}
